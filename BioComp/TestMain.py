3ca764d5bd874bf25afb412338069df9ffa2c143
WaitndSee <antoine.weber@insa-lyon.fr>
2017-12-08 10:24:29 +0100

    Update TestMain.py

--- BioComp/TestMain.py
+++ BioComp/TestMain.py
@@ -9,12 +9,11 @@ nbgenes = 10
 
 Transcript = []
 Genome = []
+Range = (0,0)
 for i in range(nbgenes):
   Transcript.append(0)
   Genome.append((0,0))
   
-print Genome
-
 def load_res():
   global Transcript
   f = open("EnregistrementTranscrits.txt","r")
@@ -22,12 +21,26 @@ def load_res():
   f.close()
   for i in range(len(lignes)):
      Transcript[i] = int(lignes[i].split(" ")[2].split("\n")[0])
-  print Transcript
+  
 
 def load_genome(directoryAddress):
-  global Genome
-  f = open("\"+directoryAddress+
-  
+  global Genome,Range
+  f = open(directoryAddress+'tousgenesidentiques.gff','r')
+  lignes = f.readlines()
+  f.close()
+  for i in range(5,len(lignes)):
+    l = lignes[i].split("\t")
+    Genome[i-5] = (int(l[3]),int(l[4]))
+  ligneRange = lignes[4].split("\t")
+  Range = (ligneRange[3],ligneRange[4])
+
+
+load_res()
+load_genome("tousgenesidentiques/")
+
+print Transcript
+print Genome
+print Range
 
 #~ def load_resultats_sym()
 
